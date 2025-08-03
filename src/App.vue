3f4d581ac15e663e8762e501e9aa8d1f0f10a497
高飞 <2011839817@qq.com>
2025-08-03 10:19:48 +0800

    feat: '🔨 use Reka UI & Tailwind 4'

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-4">
+        <AButton>Click me</AButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+import AButton from './components/button/Button.vue'
+</script>
